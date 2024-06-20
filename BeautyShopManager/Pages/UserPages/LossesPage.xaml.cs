78edcf65839c7100ed7f1edf74fa9f74beced394
Pelimenya <portalplay@mail.ru>
2024-06-20 16:10:00 +0300

    fix losses page

--- BeautyShopManager/Pages/UserPages/LossesPage.xaml.cs
+++ BeautyShopManager/Pages/UserPages/LossesPage.xaml.cs
@@ -1,54 +1,69 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows.Controls;
 using BeautyShopManager.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace BeautyShopManager.Pages.UserPages;
-
-public partial class LossesPage : Page
+namespace BeautyShopManager.Pages.UserPages
 {
-    public ObservableCollection<Loss> Losses { get; set; }
-
-    public LossesPage()
+    public partial class LossesPage : Page
     {
-        InitializeComponent();
-        Losses = new ObservableCollection<Loss>();
-        LossesDataGrid.ItemsSource = Losses;
-    }
+        public ObservableCollection<Loss> Losses { get; set; }
 
-    private void MonthPicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
-    {
-        if (MonthPicker.SelectedDate.HasValue)
+        public LossesPage()
         {
-            DateTime selectedDate = MonthPicker.SelectedDate.Value;
-            LoadLossesForMonth(selectedDate);
+            InitializeComponent();
+            Losses = new ObservableCollection<Loss>();
+            LossesDataGrid.ItemsSource = Losses;
+            PopulateYearComboBox();
         }
-    }
 
-    private void LoadLossesForMonth(DateTime selectedDate)
-    {
-        int year = selectedDate.Year;
-        int month = selectedDate.Month;
-
-        using (var context = new ContextDataBase())
+        private void PopulateYearComboBox()
         {
-            var losses = context.Losses.Where(l => l.Lossdate.Year == year && l.Lossdate.Month == month)
-                .Include(l => l.User)
-                .Select(l => new Loss
-                {
-                    Lossdate = l.Lossdate,
-                    Inventoryloss = l.Inventoryloss,
-                    Shortage = l.Shortage,
-                    Disposal = l.Disposal,
-                    User = l.User
-                })
-                .ToList();
-
-            Losses.Clear();
-            foreach (var loss in losses)
+            int currentYear = DateTime.Now.Year;
+            for (int year = currentYear - 10; year <= currentYear + 10; year++)
             {
-                Losses.Add(loss);
+                YearComboBox.Items.Add(new ComboBoxItem { Content = year.ToString(), Tag = year });
+            }
+        }
+
+        private void DateSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (MonthComboBox.SelectedItem != null && YearComboBox.SelectedItem != null)
+            {
+                int month = int.Parse(((ComboBoxItem)MonthComboBox.SelectedItem).Tag.ToString());
+                int year = int.Parse(((ComboBoxItem)YearComboBox.SelectedItem).Tag.ToString());
+                LoadLossesForMonth(year, month);
+            }
+        }
+
+        private void LoadLossesForMonth(int year, int month)
+        {
+            using (var context = new ContextDataBase())
+            {
+                var losses = context.Losses
+                                    .Where(l => l.Lossdate.Year == year && l.Lossdate.Month == month)
+                                    .Include(l => l.User)
+                                    .Select(l => new Loss
+                                    {
+                                        Lossdate = l.Lossdate,
+                                        Inventoryloss = l.Inventoryloss,
+                                        Shortage = l.Shortage,
+                                        Disposal = l.Disposal,
+                                        User = l.User,
+                                    })
+                                    .ToList();
+
+                Losses.Clear();
+                foreach (var loss in losses)
+                {
+                    Losses.Add(loss);
+                }
+                decimal totalLosses = losses.Sum(l => l.Inventoryloss + l.Shortage + l.Disposal);
+                TotalLossesTextBlock.Text = totalLosses.ToString("F2");
             }
         }
     }
+    
 }
